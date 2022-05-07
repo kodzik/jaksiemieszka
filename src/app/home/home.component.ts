dcd89372e8275fa9c198ab1bd36c63d8836988bf
kodzik <pawlopoczta@wp.pl>
2022-05-07 21:52:27 +0200

    RWD in progress / flex home.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,23 +9,23 @@ import { debounceTime, map, startWith } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
 
-  mobile: any;
+  // mobile: any;
 
   constructor() { }
 
   ngOnInit(): void {
-    if (window.screen.width <= 769) { // 768px portrait
-      // this.mobile = true;
-    }
+    // if (window.screen.width <= 769) { // 768px portrait
+    //   // this.mobile = true;
+    // }
 
   // Checks if screen size is less than 1024 pixels
-  const checkScreenSize = () => document.body.offsetWidth < 1024;
+  // const checkScreenSize = () => document.body.offsetWidth < 1024;
 
   // Create observable from window resize event throttled so only fires every 500ms
-  const screenSizeChanged$ = fromEvent(window, 'resize').pipe(debounceTime(500)).pipe(map(checkScreenSize));  
+  // const screenSizeChanged$ = fromEvent(window, 'resize').pipe(debounceTime(500)).pipe(map(checkScreenSize));  
   // Start off with the initial value use the isScreenSmall$ | async in the
   // view to get both the original value and the new value after resize.
-  this.mobile = screenSizeChanged$.pipe(startWith(checkScreenSize()))
+  // this.mobile = screenSizeChanged$.pipe(startWith(checkScreenSize()))
   }
 
 }
