e06767c305ea6a9668c5a87c27a700d1a3103093
kodzik <pawlopoczta@wp.pl>
2023-03-23 20:18:04 +0100

    Verify email completed.

--- src/app/account/verify-email/verify-email.component.ts
+++ src/app/account/verify-email/verify-email.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-verify-email',
   templateUrl: './verify-email.component.html',
-  styleUrls: ['./verify-email.component.scss']
+  styleUrls: ['./verify-email.component.scss'],
 })
 export class VerifyEmailComponent {
-
+  constructor(public authService: AuthService) {}
 }
